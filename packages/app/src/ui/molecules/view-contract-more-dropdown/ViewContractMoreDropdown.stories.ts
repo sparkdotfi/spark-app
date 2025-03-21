22b858acbd6e57e6d7eb830e664245455715eb79
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2025-03-21 09:40:52 +0100

    Add available to stake panel (#741)

---
+++ packages/app/src/ui/molecules/view-contract-more-dropdown/ViewContractMoreDropdown.stories.ts
@@ -0,0 +1,25 @@
+import { WithClassname } from '@sb/decorators'
+import { Meta, StoryObj } from '@storybook/react'
+
+import { within } from '@storybook/test'
+import { userEvent } from '@storybook/test'
+import { withRouter } from 'storybook-addon-remix-react-router'
+import { ViewContractMoreDropdown } from './ViewContractMoreDropdown'
+
+const meta: Meta<typeof ViewContractMoreDropdown> = {
+  title: 'Components/Molecules/ViewContractMoreDropdown',
+  component: ViewContractMoreDropdown,
+  decorators: [WithClassname('p-8 bg-primary flex justify-end h-48'), withRouter()],
+  args: { blockExplorerLink: '/' },
+  play: async ({ canvasElement }) => {
+    const button = await within(canvasElement).findByRole('button')
+    await userEvent.click(button)
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof ViewContractMoreDropdown>
+
+export const Default: Story = {
+  name: 'Default',
+}
