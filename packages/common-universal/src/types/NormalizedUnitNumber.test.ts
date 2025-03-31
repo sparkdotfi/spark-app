a919bb1a5e9f4e82e3814579886c81abf0a0ee8c
Bartłomiej Tarczyński <bartlomiej.tarczynski1@gmail.com>
2025-03-31 12:24:05 +0200

    Add NormalizedUnit class

--- packages/common-universal/src/types/NormalizedUnitNumber.test.ts
+++ packages/common-universal/src/types/NormalizedUnitNumber.test.ts
@@ -3,76 +3,81 @@ import { expect } from 'earl'
 import { describe, it } from 'mocha'
 import { AssertionError } from '../assert/AssertionError.js'
 import { BaseUnitNumber } from './BaseUnitNumber.js'
-import { NormalizedUnitNumber } from './NormalizedUnitNumber.js'
+import { NormalizedUnit, NormalizedUnitClass } from './NormalizedUnitNumber.js'
 
-describe(NormalizedUnitNumber.name, () => {
+describe(NormalizedUnit.name, () => {
   it('works for a numeric value', () => {
-    expect(NormalizedUnitNumber(10n ** 3n)).toEqual(new BigNumber(1000) as NormalizedUnitNumber)
-    expect(NormalizedUnitNumber(1000)).toEqual(new BigNumber(1000) as NormalizedUnitNumber)
-    expect(NormalizedUnitNumber('1000')).toEqual(new BigNumber(1000) as NormalizedUnitNumber)
-    expect(NormalizedUnitNumber(new BigNumber(1000))).toEqual(new BigNumber(1000) as NormalizedUnitNumber)
-    expect(NormalizedUnitNumber('123.456')).toEqual(new BigNumber(123.456) as NormalizedUnitNumber)
+    expect(new NormalizedUnit(10n ** 3n)).toEqual(new NormalizedUnit(1000))
+    expect(new NormalizedUnit(1000)).toEqual(new NormalizedUnit(1000))
+    expect(new NormalizedUnit('1000')).toEqual(new NormalizedUnit(1000))
+    expect(new NormalizedUnit(new BigNumber(1000))).toEqual(new NormalizedUnit(1000))
+    expect(new NormalizedUnit('123.456')).toEqual(new NormalizedUnit(123.456))
+  })
+
+  it('comparison works using constructor and non-constructor', () => {
+    expect(new NormalizedUnit(1000)).toEqual(NormalizedUnit(1000))
+  })
+
+  it('is instance of underlying class', () => {
+    expect(NormalizedUnit(10) instanceof NormalizedUnitClass).toBeTruthy()
+  })
+
+  it('stores string representation of value', () => {
+    const value = NormalizedUnit(10)
+    expect(value.asString).toEqual('10')
+    expect(value.asString).toEqual(value.toString())
   })
 
   it('works with negative numbers', () => {
-    expect(NormalizedUnitNumber(-1)).toEqual(new BigNumber(-1) as NormalizedUnitNumber)
+    expect(new NormalizedUnit(-1)).toEqual(new NormalizedUnit(-1))
   })
 
   it('throws if value argument is non-numeric value', () => {
-    expect(() => NormalizedUnitNumber('123,456')).toThrow('Value argument: 123,456 cannot be converted to BigNumber.')
-    expect(() => NormalizedUnitNumber('non-numeric')).toThrow(
+    expect(() => new NormalizedUnit('123,456')).toThrow('Value argument: 123,456 cannot be converted to BigNumber.')
+    expect(() => new NormalizedUnit('non-numeric')).toThrow(
       'Value argument: non-numeric cannot be converted to BigNumber.',
     )
   })
 
-  it(`${NormalizedUnitNumber.toString.name} does not use scientific notation for big numbers`, () => {
-    expect(NormalizedUnitNumber(10n ** 100n).toString()).toEqual(`1${'0'.repeat(100)}`)
+  it(`${NormalizedUnit.prototype.toString.name} does not use scientific notation for big numbers`, () => {
+    expect(new NormalizedUnit(10n ** 100n).toString()).toEqual(`1${'0'.repeat(100)}`)
   })
 
-  describe(NormalizedUnitNumber.toBaseUnit.name, () => {
+  describe(NormalizedUnit.prototype.toBaseUnit.name, () => {
     it('works with standard decimals', () => {
-      expect(NormalizedUnitNumber.toBaseUnit(NormalizedUnitNumber(2.5), 18)).toEqual(
-        BaseUnitNumber(2_500000000000000000n),
-      )
-      expect(NormalizedUnitNumber.toBaseUnit(NormalizedUnitNumber(10), 6)).toEqual(BaseUnitNumber(10_000000))
+      expect(NormalizedUnit(2.5).toBaseUnit(18)).toEqual(BaseUnitNumber(2_500000000000000000n))
+      expect(NormalizedUnit(10).toBaseUnit(6)).toEqual(BaseUnitNumber(10_000000))
     })
 
     it('rounds down when precision is greater than decimals', () => {
-      expect(
-        NormalizedUnitNumber.toBaseUnit(NormalizedUnitNumber('1.555555555555555555555555555555555555'), 18),
-      ).toEqual(BaseUnitNumber('1555555555555555555'))
+      expect(NormalizedUnit('1.555555555555555555555555555555555555').toBaseUnit(18)).toEqual(
+        BaseUnitNumber('1555555555555555555'),
+      )
     })
   })
 
-  describe(NormalizedUnitNumber.min.name, () => {
+  describe(NormalizedUnit.min.name, () => {
     it('returns the smallest value', () => {
-      expect(NormalizedUnitNumber.min(NormalizedUnitNumber(2), NormalizedUnitNumber(1))).toEqual(
-        NormalizedUnitNumber(1),
-      )
+      expect(NormalizedUnit.min(new NormalizedUnit(2), new NormalizedUnit(1))).toEqual(new NormalizedUnit(1))
     })
 
     it('returns the smallest value when there are more than 2 arguments', () => {
       expect(
-        NormalizedUnitNumber.min(
-          NormalizedUnitNumber(3),
-          NormalizedUnitNumber(1),
-          NormalizedUnitNumber(5),
-          NormalizedUnitNumber(2),
-        ),
-      ).toEqual(NormalizedUnitNumber(1))
+        NormalizedUnit.min(new NormalizedUnit(3), new NormalizedUnit(1), new NormalizedUnit(5), new NormalizedUnit(2)),
+      ).toEqual(new NormalizedUnit(1))
     })
 
     it('works with array', () => {
-      const input = [NormalizedUnitNumber(3), NormalizedUnitNumber(1), NormalizedUnitNumber(5), NormalizedUnitNumber(2)]
-      expect(NormalizedUnitNumber.min(...input)).toEqual(NormalizedUnitNumber(1))
+      const input = [new NormalizedUnit(3), new NormalizedUnit(1), new NormalizedUnit(5), new NormalizedUnit(2)]
+      expect(NormalizedUnit.min(...input)).toEqual(new NormalizedUnit(1))
     })
 
     it('return the argument if there is only 1 element', () => {
-      expect(NormalizedUnitNumber.min(NormalizedUnitNumber(1))).toEqual(NormalizedUnitNumber(1))
+      expect(NormalizedUnit.min(new NormalizedUnit(1))).toEqual(new NormalizedUnit(1))
     })
 
     it('throws if there are no arguments', () => {
-      expect(() => NormalizedUnitNumber.min()).toThrow(AssertionError, 'Requires at least 1 arg')
+      expect(() => NormalizedUnit.min()).toThrow(AssertionError, 'Requires at least 1 arg')
     })
   })
 })
--- packages/common-universal/src/types/NormalizedUnitNumber.ts
+++ packages/common-universal/src/types/NormalizedUnitNumber.ts
@@ -1,4 +1,4 @@
-import BigNumber from 'bignumber.js'
+import { BigNumber } from 'bignumber.js'
 import { assert } from '../assert/assert.js'
 import { NumberLike, bigNumberify } from '../math/bigNumber.js'
 import { BaseUnitNumber } from './BaseUnitNumber.js'
@@ -28,3 +28,234 @@ NormalizedUnitNumber.min = function min(...values: NormalizedUnitNumber[]): Norm
   }
   return min
 }
+
+interface NormalizedUnit {
+  readonly value: BigNumber
+  readonly asString: string
+
+  toBaseUnit(decimals: number): BaseUnitNumber
+
+  abs(): NormalizedUnit
+  eq(n: NormalizedUnit, base?: number): boolean
+  abs(): NormalizedUnit
+  comparedTo(n: NormalizedUnit, base?: number): number
+  decimalPlaces(decimalPlaces: number, roundingMode?: BigNumber.RoundingMode): NormalizedUnit
+  div(n: NormalizedUnit, base?: number): NormalizedUnit
+  pow(n: number, m?: NormalizedUnit): NormalizedUnit
+  sqrt(): NormalizedUnit
+  integerValue(rm?: BigNumber.RoundingMode): NormalizedUnit
+  eq(n: NormalizedUnit, base?: number): boolean
+  gt(n: NormalizedUnit, base?: number): boolean
+  gte(n: NormalizedUnit, base?: number): boolean
+  lt(n: NormalizedUnit, base?: number): boolean
+  lte(n: NormalizedUnit, base?: number): boolean
+  isNaN(): boolean
+  isInteger(): boolean
+  isFinite(): boolean
+  isNegative(): boolean
+  isPositive(): boolean
+  isZero(): boolean
+  minus(n: NormalizedUnit, base?: number): NormalizedUnit
+  plus(n: NormalizedUnit, base?: number): NormalizedUnit
+  mod(n: NormalizedUnit, base?: number): NormalizedUnit
+  times(n: NormalizedUnit, base?: number): NormalizedUnit
+  negated(): NormalizedUnit
+  precision(significantDigits: number, roundingMode?: BigNumber.RoundingMode): NormalizedUnit
+  shiftedBy(n: number): NormalizedUnit
+  toExponential(): string
+  toFixed(): string
+  toFormat(format: BigNumber.Format): string
+  toFraction(maxDenominator?: NormalizedUnit): [BigNumber, BigNumber]
+  toJSON(): string
+  toNumber(): number
+  toPrecision(): string
+  toString(base?: number): string
+  valueOf(): string
+}
+
+// Should be used only for type checking (e.g. instanceof)
+export class NormalizedUnitClass implements NormalizedUnit {
+  readonly value: BigNumber
+  readonly asString: string
+
+  constructor(value: NumberLike) {
+    this.value = BigNumber(bigNumberify(value))
+    this.asString = this.value.toString()
+  }
+
+  toBaseUnit(decimals: number): BaseUnitNumber {
+    const lowerPrecisionNumber = this.value.decimalPlaces(decimals, BigNumber.ROUND_DOWN)
+    return BaseUnitNumber(lowerPrecisionNumber.shiftedBy(decimals))
+  }
+
+  abs(): NormalizedUnit {
+    return new NormalizedUnit(this.value.abs())
+  }
+
+  comparedTo(n: NormalizedUnit, base?: number): number {
+    return this.value.comparedTo(n.value, base)
+  }
+
+  decimalPlaces(decimalPlaces: number, roundingMode?: BigNumber.RoundingMode): NormalizedUnit {
+    return new NormalizedUnit(this.value.decimalPlaces(decimalPlaces, roundingMode))
+  }
+
+  div(n: NormalizedUnit, base?: number): NormalizedUnit {
+    return new NormalizedUnit(this.value.div(n.value, base))
+  }
+
+  pow(n: number, m?: NormalizedUnit): NormalizedUnit {
+    return new NormalizedUnit(this.value.pow(n, m?.value))
+  }
+
+  sqrt(): NormalizedUnit {
+    return new NormalizedUnit(this.value.sqrt())
+  }
+
+  integerValue(rm?: BigNumber.RoundingMode): NormalizedUnit {
+    return new NormalizedUnit(this.value.integerValue(rm))
+  }
+
+  eq(n: NormalizedUnit, base?: number): boolean {
+    return this.value.eq(n.value, base)
+  }
+
+  gt(n: NormalizedUnit, base?: number): boolean {
+    return this.value.gt(n.value, base)
+  }
+
+  gte(n: NormalizedUnit, base?: number): boolean {
+    return this.value.gte(n.value, base)
+  }
+
+  lt(n: NormalizedUnit, base?: number): boolean {
+    return this.value.lt(n.value, base)
+  }
+
+  lte(n: NormalizedUnit, base?: number): boolean {
+    return this.value.lte(n.value, base)
+  }
+
+  isNaN(): boolean {
+    return this.value.isNaN()
+  }
+
+  isInteger(): boolean {
+    return this.value.isInteger()
+  }
+
+  isFinite(): boolean {
+    return this.value.isFinite()
+  }
+
+  isNegative(): boolean {
+    return this.value.isNegative()
+  }
+
+  isPositive(): boolean {
+    return this.value.isPositive()
+  }
+
+  isZero(): boolean {
+    return this.value.isZero()
+  }
+
+  minus(n: NormalizedUnit, base?: number): NormalizedUnit {
+    return new NormalizedUnit(this.value.minus(n.value, base))
+  }
+
+  plus(n: NormalizedUnit, base?: number): NormalizedUnit {
+    return new NormalizedUnit(this.value.plus(n.value, base))
+  }
+
+  mod(n: NormalizedUnit, base?: number): NormalizedUnit {
+    return new NormalizedUnit(this.value.mod(n.value, base))
+  }
+
+  times(n: NormalizedUnit, base?: number): NormalizedUnit {
+    return new NormalizedUnit(this.value.times(n.value, base))
+  }
+
+  negated(): NormalizedUnit {
+    return new NormalizedUnit(this.value.negated())
+  }
+
+  precision(significantDigits: number, roundingMode?: BigNumber.RoundingMode): NormalizedUnit {
+    return new NormalizedUnit(this.value.precision(significantDigits, roundingMode))
+  }
+
+  shiftedBy(n: number): NormalizedUnit {
+    return new NormalizedUnit(this.value.shiftedBy(n))
+  }
+
+  toExponential(): string {
+    return this.value.toExponential()
+  }
+
+  toFixed(): string {
+    return this.value.toFixed()
+  }
+
+  toFormat(format: BigNumber.Format): string {
+    return this.value.toFormat(format)
+  }
+
+  toFraction(maxDenominator?: NormalizedUnit): [BigNumber, BigNumber] {
+    return this.value.toFraction(maxDenominator?.value)
+  }
+
+  toJSON(): string {
+    return this.value.toJSON()
+  }
+
+  toNumber(): number {
+    return this.value.toNumber()
+  }
+
+  toPrecision(): string {
+    return this.value.toPrecision()
+  }
+
+  toString(base?: number): string {
+    return this.value.toString(base)
+  }
+
+  valueOf(): string {
+    return this.value.valueOf()
+  }
+}
+
+function NormalizedUnitFunction(value: NumberLike): NormalizedUnitClass {
+  const result = bigNumberify(value)
+  return new NormalizedUnitClass(result)
+}
+
+interface StaticNormalizedUnit {
+  new (value: NumberLike): NormalizedUnit
+  (value: NumberLike): NormalizedUnit
+  readonly prototype: NormalizedUnit
+
+  min(...values: NormalizedUnitClass[]): NormalizedUnitClass
+}
+
+const NormalizedUnitStaticFunctions: Pick<StaticNormalizedUnit, 'min'> = {
+  min(...values: NormalizedUnit[]): NormalizedUnit {
+    assert(values.length > 0, 'Requires at least 1 arg')
+    let min = values[0]!
+    for (const value of values) {
+      if (value.lt(min)) {
+        min = value
+      }
+    }
+    return min
+  },
+}
+
+Object.setPrototypeOf(NormalizedUnitFunction, NormalizedUnitClass)
+NormalizedUnitFunction.prototype = NormalizedUnitClass.prototype
+
+export const NormalizedUnit: StaticNormalizedUnit = Object.assign(
+  NormalizedUnitFunction,
+  NormalizedUnitClass,
+  NormalizedUnitStaticFunctions,
+)
