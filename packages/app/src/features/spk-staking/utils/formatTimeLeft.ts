30e11a959ac8bb4b9d05390359e84bff8afc3d4a
yivlad <jacenko.vlad@gmail.com>
2025-04-02 12:04:24 +0200

    Update format time left

--- packages/app/src/features/spk-staking/utils/formatTimeLeft.ts
+++ packages/app/src/features/spk-staking/utils/formatTimeLeft.ts
@@ -3,6 +3,6 @@ import { UnixTime } from '@sparkdotfi/common-universal'
 export function formatTimeLeft(timeLeft: UnixTime): string {
   const days = timeLeft / UnixTime.ONE_DAY()
   const hours = (timeLeft % UnixTime.ONE_DAY()) / UnixTime.ONE_HOUR()
-  const minutes = (timeLeft % UnixTime.ONE_HOUR()) / UnixTime.ONE_MINUTE()
+  const minutes = Math.ceil(Number(timeLeft % UnixTime.ONE_HOUR()) / Number(UnixTime.ONE_MINUTE()))
   return `${days}D ${hours}H ${minutes}M`
 }
